e3baeb4cdea6820c794c4c0176375814a2b0707d
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-01-21 22:30:24 -0500

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import ProjectSidebar from "./components/ProjectSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <NoProjectSelected onStartAddProject={handleStartAddProject} />
     </main>
   );
 }
