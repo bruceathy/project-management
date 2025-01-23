80cbe17e239f56b07158f09f6e63ee7d4c22c1d6
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-01-22 23:12:30 -0500

    useref setup

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,5 +1,10 @@
 import Input from "./Input";
+import { useRef } from "react";
 export default function NewProject() {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
