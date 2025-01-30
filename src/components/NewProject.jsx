b5aa362e2044eceda581b0f02c51ccf290d3bb24
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-01-29 23:30:45 -0500

    save

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -5,6 +5,8 @@ export default function NewProject() {
   const description = useRef();
   const dueDate = useRef();
 
+  function handleSave() {}
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -14,7 +16,10 @@ export default function NewProject() {
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
