e0bb68435521620f99c1348dc4373720fb186020
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-02-03 09:41:50 -0500

    update

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -5,7 +5,11 @@ export default function NewProject() {
   const description = useRef();
   const dueDate = useRef();
 
-  function handleSave() {}
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+  }
 
   return (
     <div className="w-[35rem] mt-16">
